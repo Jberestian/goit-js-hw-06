3e0c37af39d86a60398718ce54b1960a7690be4d
Jenia Berestian <jberestian@gmail.com>
2022-07-08 11:48:38 +0300

    added home work 10

--- js/task-10.js
+++ js/task-10.js
@@ -1,3 +1,25 @@
-function getRandomHexColor() {
+const randomColor = () => {
   return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
-}
+};
+
+const destroyBoxes = () => {
+  newDivEl.remove();
+};
+
+const createBoxes = () => {
+  const createDivEl = divBoxEl.append(newDivEl);
+
+  newDivEl.style.backgroundColor = randomColor();
+  newDivEl.style.margin = "auto";
+  newDivEl.style.width = "30px";
+  newDivEl.style.height = "30px";
+};
+
+const divBoxEl = document.querySelector("#boxes");
+const newDivEl = document.createElement("DIV");
+const createBtnEl = document.querySelector("button[data-create]");
+const deleteBtnEl = document.querySelector("button[data-destroy]");
+
+createBtnEl.addEventListener("click", createBoxes);
+
+deleteBtnEl.addEventListener("click", destroyBoxes);
